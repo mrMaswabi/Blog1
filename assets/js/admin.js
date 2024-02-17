a314fb5e13637c60dcf8d78266e08c5c9924f314
mrMaswabi <simanater@gmail.com>
2024-02-17 14:08:14 +1300

    upload functionality working 2

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -13,3 +13,31 @@
 
   menuIcon.addEventListener("click", toggleSidebar);
   navOverlay.addEventListener("click", toggleSidebar);
+
+
+
+  const imgBtn = document.querySelector(".img-btn");
+    const imageInput = document.querySelector(".image-input");
+    const uploadTxt = document.querySelector(".upload-txt");
+
+    imgBtn.addEventListener("click", function () {
+      // alert('clicked');
+      imageInput.click();
+    });
+    imageInput.addEventListener("change", function () {
+      const file = imageInput.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          imgBtn.style.backgroundImage = `url(${e.target.result})`;
+          imgBtn.style.height = "150px";
+          imgBtn.style.border = "none";
+          uploadTxt.classList.toggle("hide");
+        };
+
+        reader.readAsDataURL(file);
+      } else {
+        alert("didnt work");
+      }
+      // console.log({ file: imageInput.files[0]})
+    });
